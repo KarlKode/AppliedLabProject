50b519de84412534f763160ced295ccad01972f6
ph0o <leonhard.helminger@gmail.com>
2013-11-03 18:37:53 +0100

    Some more stuff

--- core/core.py
+++ core/core.py
@@ -1,11 +1,13 @@
 import logging
 import os
 import uuid
+from M2Crypto import X509, EVP, RSA, ASN1
 import Pyro4
 import sqlite3
-from OpenSSL import crypto
 from hashlib import sha1
+import time
 
+RSA_BITS = 1024
 
 PKI_DIRECTORY = "./pki"
 CRL_FILENAME = "ca.crl"
@@ -18,7 +20,6 @@ CHANGEABLE_USER_FIELDS = (
     'email',
     'pwd'
 )
-
 
 class CoreRPC(object):
     def __init__(self):
@@ -141,7 +142,7 @@ class CoreRPC(object):
             raise Exception("Invalid user")
         return self._get_certificates(user["uid"])
 
-    def create_certificate(self, session_id, title, description):
+    def create_certificate_m2(self, session_id, title, description):
         session = self._get_session(session_id)
         if session is None:
             raise Exception("Invalid session")
@@ -149,74 +150,120 @@ class CoreRPC(object):
         if user is None:
             raise Exception("Invalid user")
 
-        # Generate a new key
-        k = crypto.PKey()
-        k.generate_key(crypto.TYPE_DSA, 1024)
+        certificate = X509.Request()
 
-        certificate = crypto.X509()
-        subject = certificate.get_subject()  # TODO: We should change this
-        subject.countryName = "CH"
-        subject.stateOrProvinceName = "Zurich"
-        subject.localityName = "Zurich"
-        subject.organizationName = "iMovies"
-        subject.organizationalUnitName = "Users"
-        subject.commonName = "%s \"%s\" %s" % (user["firstname"], user["uid"], user["lastname"])
-        subject.emailAddress = user["email"]
+        # create issuer
+        subject = certificate.get_subject()
+        subject.C = "CH"
+        subject.CN = "%s \"%s\" %s" % (user["firstname"], user["uid"], user["lastname"])
+        subject.ST = 'Zurich'
+        subject.L = 'Zurich'
+        subject.O = 'iMovies'
+        subject.OU = 'Users'
 
-        certificate.set_pubkey(k)
-        #certificate.set_subject(subject)
-        certificate.set_serial_number(self._get_serial_number())  # TODO: Lock the database?
-        certificate.gmtime_adj_notBefore(0)
-        certificate.gmtime_adj_notAfter(365 * 24 * 60 * 60)  # 365 days
+        #generate RSA keypair
+        pk = EVP.PKey()
+        rsa = RSA.gen_key(RSA_BITS, 65537, lambda: None)
+        pk.assign_rsa(rsa)
 
+        #set valid-date
+        t = long(time.time())
+        now = ASN1.ASN1_UTCTIME()
+        now.set_time(t)
 
-        # TODO: Hacky shit. PLZ FIX ME!!!!!
-        ca_key = crypto.load_privatekey(crypto.FILETYPE_PEM,
-                                        file(os.path.join(PKI_DIRECTORY, KEY_FILENAME), "rb").read())
-        ca_cert = crypto.load_certificate(crypto.FILETYPE_PEM,
-                                          file(os.path.join(PKI_DIRECTORY, CERT_FILENAME), "rb").read())
+        # expires in one year from know
+        expire = ASN1.ASN1_UTCTIME()
+        expire.set_time(t + 365 * 24 * 60 * 60)
 
-        # Set certificate issuer and sign the certificate
-        certificate.set_issuer(ca_cert.get_subject())
-        certificate.sign(ca_key, "sha1")
+        certificate.set_not_before(now)
+        certificate.set_not_after(expire)
+        certificate.set_pubkey(pk)
+        certificate.set_subject(subject)
+        certificate.set_serial_number(self._get_serial_number())
 
-        certificate_data = {
-            "certificate": crypto.dump_certificate(crypto.FILETYPE_PEM, certificate),
-            "key": crypto.dump_privatekey(crypto.FILETYPE_PEM, k)
-        }
-        self._store_certificate(user["uid"], certificate_data, title, description)
-        return certificate_data
+        ca_certificate = X509.load_cert(X509.FORMAT_PEM, os.path.join(PKI_DIRECTORY, CERT_FILENAME))
+        ca_key = EVP.load_key(os.path.join(PKI_DIRECTORY, KEY_FILENAME))
 
-    def revoke_certificate(self, session_id, certificate_id):
-        session = self._get_session(session_id)
-        if session is None:
-            raise Exception("Invalid session")
-        certificate = self._get_certificate(certificate_id)
-        if certificate["uid"] != session["uid"]:
-            raise Exception("Not your certificate")
+        print ca_key
 
-        certificate_instance = crypto.load_certificate(crypto.FILETYPE_PEM, certificate["certificate"])
-        revoked = crypto.Revoked()
-        revoked.set_reason(None)  # TODO: Change this
-        revoked.set_rev_date(certificate_instance.get_notBefore())
-        revoked.set_serial(hex(certificate_instance.get_serial_number()))
-        print "lol"
-        crl = crypto.load_crl(crypto.FILETYPE_PEM, file(os.path.join(PKI_DIRECTORY, CRL_FILENAME), "rb").read())
-        print "lol"
-        crl.add_revoked(revoked)
-        print "lol"
+        return subject
 
-        # TODO: Hacky shit. PLZ FIX ME!!!!!
-        print "lol"
-        ca_key = crypto.load_privatekey(crypto.FILETYPE_PEM,
-                                        file(os.path.join(PKI_DIRECTORY, KEY_FILENAME), "rb").read())
-        ca_cert = crypto.load_certificate(crypto.FILETYPE_PEM,
-                                          file(os.path.join(PKI_DIRECTORY, CERT_FILENAME), "rb").read())
-        print "doneaaaa"
-        file(os.path.join(PKI_DIRECTORY, CRL_FILENAME), "wb").write(crl.export(ca_cert, ca_key))
-        print "done"
-
-        return self._revoke_certificate(certificate_id)
+    #def create_certificate(self, session_id, title, description):
+    #    session = self._get_session(session_id)
+    #    if session is None:
+    #        raise Exception("Invalid session")
+    #    user = self._get_user(session["uid"])
+    #    if user is None:
+    #        raise Exception("Invalid user")
+    #
+    #    # Generate a new key
+    #    k = crypto.PKey()
+    #    k.generate_key(crypto.TYPE_DSA, 1024)
+    #
+    #    certificate = crypto.X509()
+    #    subject = certificate.get_subject()  # TODO: We should change this
+    #    subject.countryName = "CH"
+    #    subject.stateOrProvinceName = "Zurich"
+    #    subject.localityName = "Zurich"
+    #    subject.organizationName = "iMovies"
+    #    subject.organizationalUnitName = "Users"
+    #    subject.commonName = "%s \"%s\" %s" % (user["firstname"], user["uid"], user["lastname"])
+    #    subject.emailAddress = user["email"]
+    #
+    #    certificate.set_pubkey(k)
+    #    #certificate.set_subject(subject)
+    #    certificate.set_serial_number(self._get_serial_number())  # TODO: Lock the database?
+    #    certificate.gmtime_adj_notBefore(0)
+    #    certificate.gmtime_adj_notAfter(365 * 24 * 60 * 60)  # 365 days
+    #
+    #
+    #    # TODO: Hacky shit. PLZ FIX ME!!!!!
+    #    ca_key = crypto.load_privatekey(crypto.FILETYPE_PEM,
+    #                                    file(os.path.join(PKI_DIRECTORY, KEY_FILENAME), "rb").read())
+    #    ca_cert = crypto.load_certificate(crypto.FILETYPE_PEM,
+    #                                      file(os.path.join(PKI_DIRECTORY, CERT_FILENAME), "rb").read())
+    #
+    #    # Set certificate issuer and sign the certificate
+    #    certificate.set_issuer(ca_cert.get_subject())
+    #    certificate.sign(ca_key, "sha1")
+    #
+    #    certificate_data = {
+    #        "certificate": crypto.dump_certificate(crypto.FILETYPE_PEM, certificate),
+    #        "key": crypto.dump_privatekey(crypto.FILETYPE_PEM, k)
+    #    }
+    #    self._store_certificate(user["uid"], certificate_data, title, description)
+    #    return certificate_data
+    #
+    #def revoke_certificate(self, session_id, certificate_id):
+    #    session = self._get_session(session_id)
+    #    if session is None:
+    #        raise Exception("Invalid session")
+    #    certificate = self._get_certificate(certificate_id)
+    #    if certificate["uid"] != session["uid"]:
+    #        raise Exception("Not your certificate")
+    #
+    #    certificate_instance = crypto.load_certificate(crypto.FILETYPE_PEM, certificate["certificate"])
+    #    revoked = crypto.Revoked()
+    #    revoked.set_reason(None)  # TODO: Change this
+    #    revoked.set_rev_date(certificate_instance.get_notBefore())
+    #    revoked.set_serial(hex(certificate_instance.get_serial_number()))
+    #    print "lol"
+    #    crl = crypto.load_crl(crypto.FILETYPE_PEM, file(os.path.join(PKI_DIRECTORY, CRL_FILENAME), "rb").read())
+    #    print "lol"
+    #    crl.add_revoked(revoked)
+    #    print "lol"
+    #
+    #    # TODO: Hacky shit. PLZ FIX ME!!!!!
+    #    print "lol"
+    #    ca_key = crypto.load_privatekey(crypto.FILETYPE_PEM,
+    #                                    file(os.path.join(PKI_DIRECTORY, KEY_FILENAME), "rb").read())
+    #    ca_cert = crypto.load_certificate(crypto.FILETYPE_PEM,
+    #                                      file(os.path.join(PKI_DIRECTORY, CERT_FILENAME), "rb").read())
+    #    print "doneaaaa"
+    #    file(os.path.join(PKI_DIRECTORY, CRL_FILENAME), "wb").write(crl.export(ca_cert, ca_key))
+    #    print "done"
+    #
+    #    return self._revoke_certificate(certificate_id)
 
     def _store_certificate(self, user_id, certificate_data, title, description):
         db = self._db_connect()
