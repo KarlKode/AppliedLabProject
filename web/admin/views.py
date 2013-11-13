eafc7fd8fb9f004b634143ee32cd3b04acfc2eae
ph0o <babsi2007!>
2013-11-13 19:07:21 +0100

    add admin panel

--- web/admin/views.py
+++ web/admin/views.py
@@ -1,8 +1,66 @@
-from flask import Blueprint
+from flask import Blueprint, g, session, render_template, redirect, Response, url_for, request
+from admin.forms import AdminLoginForm
+from utils import adminlogin_required
 
 admin_app = Blueprint("admin_app", __name__)
 
 
 @admin_app.route("/")
+@adminlogin_required
 def index():
-    return "Admin index"
+    certificates = g.rpc.get_certificates(session["admin_session_id"])
+    return render_template("admin_index.html", certificates=certificates)
+
+
+@admin_app.route("/login", methods=["GET", "POST"])
+def login():
+    form = AdminLoginForm()
+    if form.validate_on_submit():
+        try:
+            session_id = g.rpc.credential_login(form.user_id.data, form.password.data)
+            session["admin_session_id"] = session_id
+            return redirect("/")
+        except Exception as e:  # TODO: Better exception handling
+            print e.message
+            pass
+    return render_template("certlogin.html", form=form)
+
+@admin_app.route("/download/<int:certificate_id>")
+@adminlogin_required
+def download_certificate(certificate_id):
+    certificate = g.rpc.get_certificate(session["admin_session_id"], certificate_id)
+    return Response(certificate["certificate"],
+                    headers={"Content-Disposition": "attachment; filename=%s.crt" % g.user_data["uid"]},
+                    content_type="application/x-pem-file")
+
+@admin_app.route("/revoke/<int:certificate_id>")
+@adminlogin_required
+def revoke_certificate(certificate_id):
+    if "sure" in request.args:
+        test = g.rpc.revoke_certificate(session["admin_session_id"], certificate_id)
+        # TODO: Flash message!
+        return redirect(url_for("admin_app.index"))
+    certificate = g.rpc.get_certificate(session["admin_session_id"], certificate_id)
+    return render_template("revoke_certificate.html", certificate=certificate)
+
+
+@admin_app.route("/accept/<int:update_request_id>")
+@adminlogin_required
+def download_certificate(certificate_id):
+    if "sure" in request.args:
+        test = g.rpc.revoke_certificate(session["admin_session_id"], certificate_id)
+        # TODO: Flash message!
+        return redirect(url_for("admin_app.index"))
+    certificate = g.rpc.get_certificate(session["admin_session_id"], certificate_id)
+    return render_template("revoke_certificate.html", certificate=certificate)
+
+@admin_app.route("/reject/<int:update_request_id>")
+@adminlogin_required
+def revoke_certificate(certificate_id):
+    if "sure" in request.args:
+        test = g.rpc.revoke_certificate(session["admin_session_id"], certificate_id)
+        # TODO: Flash message!
+        return redirect(url_for("admin_app.index"))
+    certificate = g.rpc.get_certificate(session["admin_session_id"], certificate_id)
+    return render_template("revoke_certificate.html", certificate=certificate)
+
