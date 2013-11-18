af82a2327e5b9debbbe6b52af58c1d990699b1fd
ph0o <babsi2007!>
2013-11-18 14:40:00 +0100

    -added: verify date, verify its in the revocation list -added: flask flash messages

--- web/user/views.py
+++ web/user/views.py
@@ -19,27 +19,31 @@ def login():
                 return redirect("/")
             else:
                 flash(u'Error: ' + str(response["error"]), 'alert-danger')
-        except:
-            pass
+                return redirect("/")
+        except Exception as e:
+            flash(u'Other error: ' + str(e.message), 'alert-danger')
     return render_template("login.html", form=form)
 
 @user_app.route("/")
 @login_required
 def index():
-    r = g.rpc.get_certificates(session["session_id"])
-    if r["_rpc_status"] != "success":
-        return "Internal error", 500
+    response = g.rpc.get_certificates(session["session_id"])
+    if response["_rpc_status"] != "success":
+        flash(u'Error: ' + str(response["error"]), 'alert-danger')
 
-    certificates = r["data"]
+    certificates = response["data"]
     return render_template("user_index.html", certificates=certificates)
 
 @user_app.route("/download/<int:certificate_id>")
 @login_required
 def download_certificate(certificate_id):
-    r = g.rpc.get_certificate(session["session_id"], certificate_id)
-    if r["_rpc_status"] != "success":
-        return "Internal error", 500
-    certificate = r["data"]
+    response = g.rpc.get_certificate(session["session_id"], certificate_id)
+
+    if response["_rpc_status"] != "success":
+        flash(u'Error: ' + str(response["error"]), 'alert-danger')
+        return redirect("/")
+
+    certificate = response["data"]
     return Response(certificate["certificate"],
                     headers={"Content-Disposition": "attachment; filename=%s.crt" % g.user_data["uid"]},
                     content_type="application/x-pem-file")
@@ -51,7 +55,9 @@ def create_certificate():
     if form.validate_on_submit():
         r = g.rpc.create_certificate(session["session_id"], form.title.data, form.description.data)
         if r["_rpc_status"] != "success":
-            return "Internal error", 500
+            flash(u'Error: ' + str(r["error"]), 'alert-danger')
+            return redirect("/")
+
         certificate = r["data"]
         return Response(base64.b64decode(certificate["pkcs12"]),
                         headers={"Content-Disposition": "attachment; filename=%s.p12" % g.user_data["uid"]},
@@ -67,7 +73,9 @@ def verify_certificate():
     if form.validate_on_submit():
         r = g.rpc.verify_certificate(form.certificate.data)
         if r["_rpc_status"] != "success":
-            return "Internal error", 500
+            flash(u'Error: ' + str(r["error"]), 'alert-danger')
+            return render_template("verify_certificate.html", form=form)
+
         verification_data = r["data"]
         return render_template("verified_certificate.html", verification_data=verification_data)
     return render_template("verify_certificate.html", form=form)
@@ -77,12 +85,18 @@ def verify_certificate():
 @login_required
 def revoke_certificate(certificate_id):
     if "sure" in request.args:
-        test = g.rpc.revoke_certificate(session["session_id"], certificate_id)
-        # TODO: Flash message!
+        revoke_response = g.rpc.revoke_certificate(session["session_id"], certificate_id)
+        if revoke_response["_rpc_status"] != "success":
+            flash(u'Error: ' + str(revoke_response["error"]), 'alert-danger')
+        else:
+            flash(u'Successfully revoked certificate', 'alert-success')
+
         return redirect(url_for("user_app.index"))
+
     r = g.rpc.get_certificate(session["session_id"], certificate_id)
     if r["_rpc_status"] != "success":
-        return "Internal error", 500
+        flash(u'Error: ' + str(r["error"]), 'alert-danger')
+
     certificate = r["data"]
     return render_template("revoke_certificate.html", certificate=certificate)
 
